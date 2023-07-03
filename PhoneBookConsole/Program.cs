e184133807b6e8ad4b9bbce573de69e9102a7c74
AllenOtto <kephamoturi@gmail.com>
2023-07-03 11:04:42 +0300

    Initial commit; Creating a Phonebook Console App. Created a PhoneBook project. Created a Contact class and a Phonebook class. Added a constructor to the Contact class with getters and setters for the Name and Number properties

---
+++ PhoneBookConsole/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PhoneBookConsole;
+
+Contact contact1 = new Contact("Kepha Ototo", "0728659724");
+
+Console.WriteLine($"Name: {contact1.Name}");
+Console.WriteLine($"Number: {contact1.Number}");
